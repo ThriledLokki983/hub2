a02efcd5a4fe7ec925f9cd9ff8780142158128c7
Gideon Nimoh <gnimoh001@gmail.com>
2025-10-01 16:38:47 +0200

    Add PERSONAL/STUDY C# learning materials and essential tools

---
+++ PERSONAL/STUDY/2025/Csharp/Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your first number:");
+        // Get user input and stores it
+        int firstInputValue = int.Parse(Console.ReadLine());
+        Console.WriteLine("You entered: " + firstInputValue);
+        
+        Console.WriteLine("Enter your second number");
+        int secondInputValue = int.Parse(Console.ReadLine());
+        Console.WriteLine("You entered: " + secondInputValue);
+
+        if (int.IsPositive(firstInputValue) && int.IsPositive(secondInputValue))
+        {
+            var finalNumber = firstInputValue +  secondInputValue;
+            Console.WriteLine($"The final number is: {finalNumber}");
+        }
+        else
+        {
+            Console.WriteLine("You entered an invalid number");
+        }
+        
+        
+        Console.ReadKey();
+    }
+}
---
+++ PERSONAL/STUDY/2025/Csharp/TestApp/TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // declare a variable
+        string myFriendsName;
+        myFriendsName = "Gideon";
+        
+        // Use the variable
+        Console.WriteLine(myFriendsName);
+
+        myFriendsName = "Emmanuel";
+
+        Console.WriteLine(myFriendsName);
+        Console.ReadKey();
+    }
+}
